9b52311ba6fc25eab7c1dd9ab0444ffb724fba71
prove3305 <noname0520@naver.com>
2024-10-02 04:33:19 +0900

    week3 10번 문제

--- src/Question10.java
+++ src/Question10.java
@@ -7,7 +7,9 @@ public class Question10 {
         System.out.print("숫자를 입력하세요: ");
         int num = sc.nextInt();
 
-        // 조건 확인
+        //number % 3 == 0 && number % 5 == 0: 숫자가 3과 5의 배수인 경우.
+        //number % 3 == 0: 숫자가 3의 배수이지만 5의 배수가 아닌 경우.
+        //number % 5 == 0: 숫자가 5의 배수이지만 3의 배수가 아닌 경우.
         if (num % 3 == 0 && num % 5 == 0) {
             System.out.println("3과 5의 배수입니다.");
         } else if (num % 3 == 0) {
@@ -17,7 +19,7 @@ public class Question10 {
         } else {
             System.out.println("해당 조건을 만족하지 않습니다.");
         }
-        //
+
 
     }
 }
