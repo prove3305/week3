332935f721b19035d4146df8fcc1930684130936
prove3305 <noname0520@naver.com>
2024-10-02 04:30:05 +0900

    week3 문제

--- src/Question9.java
+++ src/Question9.java
@@ -10,7 +10,29 @@ public class Question9 {
         System.out.println("연산자를 입력하세요(+, -, *, /):");
         char operator = sc.next().charAt(0);
 
+        System.out.println("두 번째 숫자를 입력하세요:");
+        double num2= sc.nextDouble();
 
+        double result = 0;
+
+        if(operator == '+') {
+            result = num1 + num2;
+        } else if(operator == '-') {
+            result = num1 - num2;
+        } else if (operator == '*') {
+            result = num1 * num2;
+        } else if (operator == '/') {
+            if (num2 != 0) {
+                result = num1 / num2;
+            } else {
+                System.out.println("0으로 나눌 수 없습니다.");
+                return;
+            }
+        } else {
+            System.out.println("잘못된 연산자입니다.");
+        }
+
+        System.out.println("결과:" + result);
 
     }
 }
